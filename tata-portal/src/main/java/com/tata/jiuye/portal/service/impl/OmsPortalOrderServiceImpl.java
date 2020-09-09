496b7b4c865840fd510c73647c6b674012248733
cwp794387921 <cwp794387921@qq.com>
2020-09-09 22:39:01 +0800

    修改配送单列表返回字段

--- tata-portal/src/main/java/com/tata/jiuye/portal/service/impl/OmsPortalOrderServiceImpl.java
+++ tata-portal/src/main/java/com/tata/jiuye/portal/service/impl/OmsPortalOrderServiceImpl.java
@@ -537,9 +537,7 @@ public class OmsPortalOrderServiceImpl implements OmsPortalOrderService {
         //distribution.setStatus(0);//待配送
         //distribution.setType(1);//配送单
         List<OmsDistribution> list= DistributionMapper.queryList(distribution);
-        if(list!=null){
-            result.put("list",list);
-        }
+        result.put("list",list);
         return result;
     }
 
