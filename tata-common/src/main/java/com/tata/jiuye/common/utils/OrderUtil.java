7d55dce2da4f22840f465028356dbb2df689396a
coyzund/coyzund <a6757671>
2020-09-16 17:36:16 +0800

    更新订单工具类

--- tata-common/src/main/java/com/tata/jiuye/common/utils/OrderUtil.java
+++ tata-common/src/main/java/com/tata/jiuye/common/utils/OrderUtil.java
@@ -9,6 +9,7 @@ public class OrderUtil {
 	 private static String date ;
 	 private static String BHorderHead="BH";
 	 private static String CHorderHead="CH";
+    private static String TKorderHead="TK";
 
 	 /**
      * 生成补货单编号
@@ -38,6 +39,17 @@ public class OrderUtil {
         return CHorderHead+orderNo;
     }
 
+    public static synchronized String getTKOrderNo() {
+        String str = new SimpleDateFormat("yyMMddHHmmssSSS").format(new Date());
+        if(date==null||!date.equals(str)){
+            date = str;
+//            orderNum  = 0l;
+        }
+        orderNum ++;
+        long orderNo = Long.parseLong((date)) * 100;
+        orderNo += orderNum;
+        return TKorderHead+orderNo;
+    }
 
 
 }
