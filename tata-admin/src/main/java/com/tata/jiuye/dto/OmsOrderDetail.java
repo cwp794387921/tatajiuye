90debb3f0ea140de6e4f9341b63656241e226b9e
laichenhui <yangguanshuangxue@163.com>
2020-09-23 15:58:06 +0800

    订单详情增加字段 配送单号  配送中心ID(接单人)  配送中心昵称(接单人)

--- tata-admin/src/main/java/com/tata/jiuye/dto/OmsOrderDetail.java
+++ tata-admin/src/main/java/com/tata/jiuye/dto/OmsOrderDetail.java
@@ -4,6 +4,7 @@ import com.tata.jiuye.model.OmsOrder;
 import com.tata.jiuye.model.OmsOrderItem;
 import com.tata.jiuye.model.OmsOrderOperateHistory;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,13 +15,20 @@ import java.util.List;
  *
  * @author lewis
  */
+@Data
 public class OmsOrderDetail extends OmsOrder {
-    @Getter
-    @Setter
+
+    @ApiModelProperty("配送单号")
+    private Long omsDistributionNo;
+
+    @ApiModelProperty("配送中心ID(接单人)")
+    private Long wmsMemberId;
+
+    @ApiModelProperty("配送中心昵称(接单人)")
+    private String wmsMemberNickName;
+
     @ApiModelProperty("订单商品列表")
     private List<OmsOrderItem> orderItemList;
-    @Getter
-    @Setter
     @ApiModelProperty("订单操作记录列表")
     private List<OmsOrderOperateHistory> historyList;
 }
