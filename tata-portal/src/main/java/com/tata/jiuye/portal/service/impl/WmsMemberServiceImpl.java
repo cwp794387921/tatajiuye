4a3b72eb837aa2c4b9c81db8baf61059855cb30f
coyzund/coyzund <a6757671>
2020-10-02 15:01:53 +0800

    注册通知上级

--- tata-portal/src/main/java/com/tata/jiuye/portal/service/impl/WmsMemberServiceImpl.java
+++ tata-portal/src/main/java/com/tata/jiuye/portal/service/impl/WmsMemberServiceImpl.java
@@ -384,6 +384,7 @@ public class WmsMemberServiceImpl implements WmsMemberService {
         creditLineChange(wmsMember.getId(), changeValue.multiply(new BigDecimal(-1)), remark);
         wmsMemberMapper.updateByPrimaryKey(changeInfo);
         creditLineChange(changeInfo.getId(), changeValue, remark);
+
     }
 
     @Override
