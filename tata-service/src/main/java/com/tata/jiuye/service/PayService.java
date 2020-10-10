72e1a7bc6300af0398a4292beefc207c953fee1a
cwp794387921 <cwp794387921@qq.com>
2020-10-10 09:33:09 +0800

    删除service

--- tata-service/src/main/java/com/tata/jiuye/service/PayService.java
+++
@@ -1,8 +0,0 @@
-package com.tata.jiuye.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface PayService {
-      void refundApply(String orderNum,String refundMoney);
-}
